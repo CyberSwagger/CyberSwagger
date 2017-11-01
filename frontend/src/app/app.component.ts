8ad3446d4565cbb88dc92074d3d8483cb2c96a7f
Luke Wood <lukewoodcs@gmail.com>
2017-11-01 17:22:08 -0500

    Page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './templates/security-page.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
