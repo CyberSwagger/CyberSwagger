a13b09889afacbd00e5a55bdd092bce1e707d3d2
estephanian <estephanian@smu.edu>
2017-11-04 20:36:34 -0500

    quick fix to the module

--- frontend/src/app/cyber/cyber.module.ts
+++ frontend/src/app/cyber/cyber.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { UserSecurityComponent } from './user-security/user-security.component';
+import { HomeComponent } from './home/home.component';
 
 import { DomainModule } from '../domain';
 
 @NgModule({
     imports: [ BrowserModule, FormsModule, DomainModule ],
-    declarations: [ LoginComponent, UserSecurityComponent ],
+    declarations: [ LoginComponent, UserSecurityComponent, HomeComponent ],
     // bootstrap: [ ProductDetailsComponent, ProductReviewComponent, RatingComponent ],
     exports: [ LoginComponent ]
 })
