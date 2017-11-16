a50fc0d8d8d52f79e0b02cfd8bcff00ba87110b3
Ubuntu <ubuntu@ip-172-31-27-238.us-east-2.compute.internal>
2017-11-16 17:53:23 +0000

    added routes for new users

--- backend/apacheWebServer/NEApacheServer/src/routes.php
+++ backend/apacheWebServer/NEApacheServer/src/routes.php
@@ -19,3 +19,24 @@ $app->get('/[{name}]', function (Request $request, Response $response, array $ar
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+    // Add a new user
+    $app->post('/users', function ($request, $response) {
+        //$input = $request->getParsedBody();
+	$name = $_POST['name']
+	$email = $_POST['email']
+	$username = $_POST['username']
+	$phone = $_POST['phone']
+	$address = $_POST['address']
+	$password = $_POST['password']
+        $sql = "INSERT INTO users (name, email, username, phone, address, password) VALUES (:name, :email, :username, :phone, :address, :password)";
+        $sth = $this->db->prepare($sql);
+        $sth->bindParam("name", $input['name']);
+	$sth->bindParam("email", $input['email'];
+	$sth->bindParam("username", $input['username'];
+	$sth->bindParam("phone", $input['phone']);
+	$sth->bindParam("address", $input['address']);
+	$sth->bindParam("password", $input['password']);
+        $sth->execute();
+        $input['id'] = $this->db->lastInsertId();
+        return $this->response->withJson($input);
+    });
