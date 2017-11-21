0bcd37340128bed698e4692e38529a6a96373e52
Ubuntu <ubuntu@ip-172-31-27-238.us-east-2.compute.internal>
2017-11-21 18:10:01 +0000

    threats endpoint added

--- backend/apacheWebServer/NEApacheServer/src/routes.php
+++ backend/apacheWebServer/NEApacheServer/src/routes.php
@@ -12,9 +12,14 @@ $app->get('/test', function ($request, $response, $args) {
 	$test = $sth->fetchAll();
 	return $this->response->withJson($test);
 });
+//$app->get('/login', function($request, $response, $args){
+//	$this->logger->info("/login route");
+//	$username = $_GET['username'];
+//	$password = $_GET['password'];
+//	$sth = $this->db->prepart("SELECT username, password FROM users");
+//});
  // Add a new user
- $app->post('/users', function ($request, $response) {
-     //$input = $request->getParsedBody();
+ $app->post('/subscribe', function ($request, $response) {
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$username = $_POST['username'];
@@ -30,9 +35,26 @@ $app->get('/test', function ($request, $response, $args) {
 	$sth->bindParam("address", $address);
 	$sth->bindParam("password", $password);
         $sth->execute();
-       // $input['id'] = $this->db->lastInsertId();
         return $this->response->withJson($name);
  });
+//add new threat
+$app->post('/threats', function($request, $response){
+	$user_id = $_POST['user_id']
+	$location = $_POST['location']
+	$type = $_POST['type']
+	$severity = $_POST['severity']
+	$description = $_POST['description']
+	$date = $_POST['date']
+	$sql = "INSERT INTO threats (user_id, location, type, severity, description, date) VALUES (:user_id, :location, :type, :severity, :description, :date)";
+	$sth = $this->db->prepare($sql);
+	$sth->bindParam("user_id", $user_id);
+	$sth->bindParam("location",$location);
+	$sth->bindParam("type",$type);
+	$sth->bindParam("severity",$severity);
+	$sth->bindParam("description",$description);
+	$sth->bindParam("date",$date);
+	$sth->execute();
+});
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
