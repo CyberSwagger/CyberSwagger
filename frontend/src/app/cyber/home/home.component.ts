b722717954ebe4a65354c14e8b3a2a777c05b9e9
Luke Wood <lukewoodcs@gmail.com>
2017-11-29 13:51:36 -0600

    post done

--- frontend/src/app/cyber/home/home.component.ts
+++ frontend/src/app/cyber/home/home.component.ts
@@ -3,6 +3,7 @@ import { User, Threat } from '../../domain';
 import { NavComponent } from '../navbar/navbar.component';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'home',
@@ -19,12 +20,34 @@ export class HomeComponent {
   threatpostLogo: string;
   lockImg: string;
 
-constructor(private route: ActivatedRoute) {
+  city: string;
+  state: string;
+  zip: string;
+  severity: string;
+  attack_type: string;
+  description: string;
+  attack_date: string;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.imageUrl = '/assets/images/';
     this.hackerLogo = 'hacker.png';
     this.inverseLogo = 'inverse.png';
     this.threatpostLogo = 'threatpost.png';
     this.lockImg = 'lock.png';
+  }
+
+  reportThreat() {
+    let threat = {
+      city: this.city,
+      state : this.state,
+      zip: this.zip,
+      severity: this.severity,
+      type: "Trojan",
+      description: this.description
+    }
+
+    this.http.post('http://35.164.245.5/threats/addthreats', threat);
+
   }
 
 }
